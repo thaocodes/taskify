347a9fc1178288bccc6785ae82f31c9d4cb447df
thao <thaole0325@gmail.com>
2023-09-12 10:31:38 -0400

    each todo item is now draggable

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -2,16 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from './model';
 import { AiFillEdit, AiFillDelete  } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import { Draggable } from 'react-beautiful-dnd';
 import "./styles.css";
 
 
 type Props = {
+    index: number;
     todo: Todo,    // what we defined in model.ts
     todos: Todo[],   // array of Todo
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+const SingleTodo: React.FC<Props> = ({ index, todo, todos, setTodos }) => {
     // 2 states for `edit` 
     const [edit, setEdit] = useState<boolean>(false);   // keeps track if edit mode is on or not
     // keeps track of the value of edited `todo`
@@ -59,53 +61,66 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     };
 
     return (
-        // submit even for editing task, takes event object & todo.id
-        <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+        // make single todo draggable 
+        // provide unique id (todo.id), index we get from TodoList to keep track of index
+        <Draggable draggableId={todo.id.toString()} index={index}> 
+            {/* add function, pass it provided */}
+            {(provided) => (
+            //  submit event for editing task, takes event object & todo.id 
+            <form // pass provided to parent element, provide props & ref
+                className="todos__single" 
+                onSubmit={(e) => handleEdit(e, todo.id)}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+            >
             {/* if edit mode is ON, then display input box */}
-            {edit ? (
-                <input 
-                    ref={inputRef}
-                    value={editTodo}
-                    // update the state of edited `todo` with what user inputs
-                    onChange={(e) => setEditTodo(e.target.value)}
-                    className="todos__single--text"
-                />
-            // otherwise, display normal `todo`
+                {edit ? (
+                    <input 
+                        ref={inputRef}
+                        value={editTodo}
+                        // update the state of edited `todo` with what user inputs
+                        onChange={(e) => setEditTodo(e.target.value)}
+                        className="todos__single--text"
+                    />
+                // otherwise, display normal `todo`
 
-            //  if todo is completed, strike it off 
-            ) : todo.isDone ? (   
-                //  <s is a strike tag
-                <s className="todos__single--text">{todo.todo}</s>     
-            ) : (
-                // otherwise, render normal `todo`
-                <span className="todos__single--text">{todo.todo}</span>
+                //  if todo is completed, strike it off 
+                ) : todo.isDone ? (   
+                    //  <s is a strike tag
+                    <s className="todos__single--text">{todo.todo}</s>     
+                ) : (
+                    // otherwise, render normal `todo`
+                    <span className="todos__single--text">{todo.todo}</span>
+                )}
+                    
+                <div>
+                    <span 
+                        className="icon" 
+                        // cannot edit task that is already marked Done
+                        onClick={() => {
+                        // if edit mode is NOT on/true (i.e edit is false) & todo is NOT done
+                            if(!edit && !todo.isDone) {
+                                // change `edit` to oppositie value (true)
+                                setEdit(!edit)  // initial state of `edit` = false
+                            }
+                        }}
+                    >
+                    
+                        <AiFillEdit />
+                    </span>
+                    <span className="icon" onClick={() => handleDelete(todo.id)}>
+                        <AiFillDelete />
+                    </span>
+                    {/* fire off handleDone function & send it todo.id */}
+                    <span className="icon" onClick={() => handleDone(todo.id)}>
+                        <MdDone />
+                    </span>
+                </div>
+                { todo.todo }
+            </form>
             )}
-                
-            <div>
-                <span 
-                    className="icon" 
-                    // cannot edit task that is already marked Done
-                    onClick={() => {
-                    // if edit mode is NOT on/true (i.e edit is false) & todo is NOT done
-                        if(!edit && !todo.isDone) {
-                            // change `edit` to oppositie value (true)
-                            setEdit(!edit)  // initial state of `edit` = false
-                        }
-                    }}
-                >
-                
-                    <AiFillEdit />
-                </span>
-                <span className="icon" onClick={() => handleDelete(todo.id)}>
-                    <AiFillDelete />
-                </span>
-                {/* fire off handleDone function & send it todo.id */}
-                <span className="icon" onClick={() => handleDone(todo.id)}>
-                    <MdDone />
-                </span>
-            </div>
-            { todo.todo }
-        </form>
+        </Draggable>
     )
 }
 
