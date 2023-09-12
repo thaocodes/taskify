2b151a88f60cac9e5c6a7db03c77d7c1c610af04
thao <thaole0325@gmail.com>
2023-09-12 09:34:47 -0400

    commented out useReducer implementation, save for furture refactoring

--- src/components/model.ts
+++ src/components/model.ts
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+// import { useReducer } from "react";
 
 export interface Todo {
     id: number;
@@ -13,38 +13,38 @@ export interface Todo {
 
 // create type for Actions
 // each action has a type & payload 
-type Actions = 
-    | { type: "add", payload: string }    // payload = string we send from input box 
-    | { type: "remove", payload: number } // payload sends id 
-    | { type: "done", payload: number  };  // payload sends id 
+// type Actions = 
+//     | { type: "add", payload: string }    // payload = string we send from input box 
+//     | { type: "remove", payload: number } // payload sends id 
+//     | { type: "done", payload: number  };  // payload sends id 
 
-// reducer takes a state & actions. NEED to provide TYPES to state & actions 
-// state = array of todos
-// actions = add, remove, done 
-const TodoReducer = (state: Todo[], action:Actions) => {
-    // switch case
-    switch (action.type) {
-        case "add":
-            return [
-                ...state,
-                { id: Date.now(), todo: action.payload, isDone: false },
-            ];
-        case "remove": 
-            return state.filter((todo) => todo.id !== action.payload);
-        case "done":
-            return state.map((todo) => 
-            todo.id !== action.payload ? {...todo, isDone: !todo.isDone }: todo);
-        default:
-            return state;
-    }
-}
+// // reducer takes a state & actions. NEED to provide TYPES to state & actions 
+// // state = array of todos
+// // actions = add, remove, done 
+// const TodoReducer = (state: Todo[], action:Actions) => {
+//     // switch case
+//     switch (action.type) {
+//         case "add":
+//             return [
+//                 ...state,
+//                 { id: Date.now(), todo: action.payload, isDone: false },
+//             ];
+//         case "remove": 
+//             return state.filter((todo) => todo.id !== action.payload);
+//         case "done":
+//             return state.map((todo) => 
+//             todo.id !== action.payload ? {...todo, isDone: !todo.isDone }: todo);
+//         default:
+//             return state;
+//     }
+// }
 
 
-// useReducer: provide it initial value (reducer) returns a state & dispatch
-const ReducerExample = () => {
-    const [state, dispatch] = useReducer(TodoReducer, [])
+// // useReducer: provide it initial value (reducer) returns a state & dispatch
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, [])
 
-    return (
-        <div/>
-    )
-}
+//     return (
+//         <div/>
+//     )
+// }
